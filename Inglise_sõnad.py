63c4c85f1f30b6ed78e0199153be74ea4f31fdcb
KM100 <34981844+KM100@users.noreply.github.com>
2018-01-06 15:00:50 +0200

    Add files via upload
    Tegin midagi

---
+++ Inglise_sõnad.py
@@ -0,0 +1,10 @@
+sõnad = ['koer', 'dog', 'isa', 'dad/father', 'ema', 'mom/mother', 'seal', 'there', 'puit', 'wood', 'karussell', 'carousel', 'piim', 'milk', 'kass', 'cat', 'voodi', 'bed', 'jõuluvana', 'santa', 'auto', 'car', 'uks', 'door', 'salaja', 'secretly', 'arvuti', 'computer', 'hommik', 'morning', 'tere', 'hello/hi', 'pere', 'family', 'laud', 'table', 'hiir', 'mouse', 'tsau', 'bye', 'sõna', 'word', 'pudel', 'bottle', 'leib', 'bread', 'lukk', 'lock', 'sein', 'wall', 'pakk', 'package', 'nimi', 'name', 'mis', 'what', 'kartul', 'potato', 'porgand', 'carrot', 'raamat', 'book', 'lumi', 'snow', 'mitu', 'how many', 'palju', 'a lot of .../lots of ...', '0', 'zero', '1', 'one', '2', 'two', 'pirukas', 'pie', 'paat', 'boat', 'kirves', 'axe', 'tekst', 'text', 'inimene', 'person', 'sa', 'you', 'ma', 'I', 'pilt', 'picture', 'kunst', 'art', 'eriline', 'special', 'tee', 'road/tea', 'pikk', 'tall', 'lühike', 'short', 'päev', 'day']
+print('Sõnastikus on '+str(int(len(sõnad)/2))+' sõna.')
+##while True:
+##    sõna = input('Sõna tõlkimiseks: ')
+##    if sõna in sõnad:
+##        for i in range(len(sõnad)):
+##            if sõnad[i] == sõna:
+##                print('Tõlge sellele on "'+sõnad[i+1]+'".')
+##    else:
+##        print('Sõna puudub.')
